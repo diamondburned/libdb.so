38fa745fd3a91158f09a6097b3fbf4a06a8d218a
diamondburned <diamond@arikawa-hi.me>
2023-05-09 13:04:04 -0700

    Implement httpfs timeout

--- vm/rwfs/httpfs/http.go
+++ vm/rwfs/httpfs/http.go
@@ -1,9 +1,11 @@
 package httpfs
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"path"
+	"time"
 )
 
 type httpClient struct {
@@ -11,10 +13,22 @@ type httpClient struct {
 	basePath string
 }
 
+// Timeout is the default timeout for http requests.
+const Timeout = 8 * time.Second
+
 func (c *httpClient) get(filepath string, info FileInfo) (io.ReadCloser, error) {
 	filepath = path.Join(c.basePath, filepath)
 
-	resp, err := c.client.Get(filepath)
+	ctx, cancel := context.WithTimeout(context.Background(), Timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, filepath, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer req.Body.Close()
+
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
