d488429adfdede0f503fbe2a728e386e3e8d3f6e
diamondburned <diamond@arikawa-hi.me>
2023-11-06 04:05:20 -0800

    Add botton nav bar
    Also add an empty WIP portfolio page. The nav bar also has a clock.
    
    Fixes #2.

--- site/App.svelte
+++ site/App.svelte
@@ -5,6 +5,32 @@
   import type * as xterm from "xterm";
 
   import Terminal from "#/libdb.so/site/components/Terminal/index.svelte";
+  import Portfolio from "#/libdb.so/site/components/Portfolio/index.svelte";
+
+  type View = null | "terminal" | "portfolio";
+
+  let currentView: View = "terminal";
+  let lastView: View = null;
+  function switchView(view: View) {
+    currentView = currentView == view ? null : view;
+    lastView = null;
+  }
+  function viewDesktop() {
+    if (!lastView) lastView = currentView;
+    currentView = currentView == null ? lastView : null;
+  }
+
+  let currentTime = "00:00";
+  function updateTime() {
+    currentTime = new Date().toLocaleTimeString(undefined, {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  }
+  updateTime();
+
+  const updateTimer = setInterval(() => updateTime(), 5000);
+  svelte.onDestroy(() => clearInterval(updateTimer));
 </script>
 
 <svelte:head>
@@ -26,17 +52,53 @@
 </svelte:head>
 
 <main>
-  <div class="backdrop">
-    <Terminal
-      id="terminal"
-      done={(terminal) => {
-        vm.start(terminal, "/_fs.json").catch((err) => console.error(err));
-      }}
-    />
+  <div class="backdrop" />
+
+  <div class="content">
+    <div class="centered" class:active={currentView == "terminal"}>
+      <Terminal
+        id="terminal"
+        done={(terminal) => {
+          vm.start(terminal, "/_fs.json").catch((err) => console.error(err));
+        }}
+      />
+    </div>
+    <div class:active={currentView == "portfolio"}>
+      <Portfolio />
+    </div>
   </div>
+
+  <nav id="navbar">
+    <div class="left">
+      <button class="start" on:click={() => alert("hii!!")}>
+        <img src="/favicon.ico" alt="diamondburned's eye" />
+      </button>
+      <div class="window-list">
+        <button
+          class:active={currentView == "portfolio"}
+          on:click={() => switchView("portfolio")}
+          disabled
+        >
+          <img src="/papirus/system-users.svg" alt="diamondburned's eye" />
+          Portfolio
+        </button>
+        <button
+          class:active={currentView == "terminal"}
+          on:click={() => switchView("terminal")}
+        >
+          <img src="/papirus/utilities-terminal.svg" alt="Terminal" />
+          Terminal
+        </button>
+      </div>
+    </div>
+    <div class="right">
+      <span class="clock">{currentTime}</span>
+      <button class="view-desktop" on:click={() => viewDesktop()} />
+    </div>
+  </nav>
 </main>
 
-<style global>
+<style global lang="scss">
   body {
     font-family: "Lato", "Source Sans Pro", "Noto Sans", "Helvetica", "Segoe UI",
       sans-serif;
@@ -54,26 +116,151 @@
     background-color: dimgray;
     background-image: url("/_fs/Pictures/background.jpg");
     background-size: cover;
-  }
-
-  main > div {
-    width: 100%;
-    height: 100%;
 
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
 
-    backdrop-filter: blur(20px);
+    .content {
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+
+      & > div {
+        transition: opacity 0.075s ease-in-out;
+
+        &:not(.active) {
+          opacity: 0;
+          pointer-events: none;
+        }
+      }
+    }
+
+    .centered {
+      width: 100%;
+      height: 100%;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    & > * {
+      z-index: 1;
+    }
+
+    & > .backdrop {
+      width: 100%;
+      height: 100%;
+      z-index: 0;
+      position: absolute;
+      backdrop-filter: blur(20px);
+    }
+  }
+
+  #navbar {
+    background-color: rgba(0, 0, 0, 0.5);
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.25),
+      rgba(0, 0, 0, 0.65)
+    );
+
+    display: flex;
+    justify-content: space-between;
+    gap: 0.5rem;
+
+    font-weight: 700;
+    user-select: none;
+
+    .left {
+      width: 100%;
+    }
+
+    .left,
+    .right {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .window-list {
+      width: 100%;
+
+      display: grid;
+      grid-template-columns: repeat(auto-fit, clamp(100px, 20%, 200px));
+      grid-template-rows: 1fr;
+
+      overflow-x: auto;
+    }
+
+    button {
+      --bg-hover: rgba(255, 255, 255, 0.05);
+      --bg-active: rgba(255, 255, 255, 0.1);
+      --bg-active-hover: rgba(255, 255, 255, 0.15);
+
+      border: none;
+      background-color: transparent;
+      font-weight: inherit;
+      color: white;
+
+      display: flex;
+      align-items: center;
+
+      padding: 0.35em 0.5em;
+      padding-left: 0;
+
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid transparent;
+
+      &:hover:not(:disabled) {
+        background-color: var(--bg-hover);
+      }
+
+      &:disabled {
+        opacity: 0.5;
+      }
+
+      &.active {
+        background-color: var(--bg-active);
+        border-bottom: 2px solid white;
+
+        &:hover {
+          background-color: var(--bg-active-hover);
+        }
+      }
+
+      img {
+        width: 1.5em;
+        height: 1.5em;
+        margin: 0 0.5em;
+      }
+    }
+
+    .start {
+      padding-right: 0;
+    }
+
+    .clock {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      padding: 0 0.5rem;
+    }
+
+    .view-desktop {
+      width: 0.35em;
+      border-left: 1px solid rgba(255, 255, 255, 0.25);
+
+      &:hover {
+        background-color: var(--bg-hover);
+      }
+    }
   }
 
   #terminal {
     width: min(calc(100% - clamp(6px, 5vw, 3rem)), max(80vw, 1000px));
     height: min(calc(100% - clamp(6px, 7vw, 5rem)), max(70vh, 800px));
-  }
 
-  @media (max-width: 500px) {
-    #terminal {
+    @media (max-width: 500px) {
       width: 100%;
       height: 100%;
       border-radius: 0;
