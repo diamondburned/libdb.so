98e01c25ea64dc292e223d2acf85fc0ac2b2775c
diamondburned <diamond@arikawa-hi.me>
2023-11-08 04:45:08 -0800

    Add Window component, support maximizing

--- site/components/Terminal/index.svelte
+++ site/components/Terminal/index.svelte
@@ -5,19 +5,22 @@
   import colorScheme from "./color-schemes.json";
   import type * as xterm from "xterm";
 
+  import Window from "#/libdb.so/site/components/Window.svelte";
+
   let terminalElement: HTMLElement;
 
   export let id: string;
   export let done: (_: xterm.Terminal) => void;
   export let colors: Record<string, string> = {};
 
-  let title = "libdb.so";
+  let title = "";
+  let terminal: xterm.Terminal;
   $: combinedColors = { ...colorScheme, ...colors };
 
   svelte.onMount(async () => {
     const libterminal = await import("#/libdb.so/site/lib/terminal.js");
 
-    const terminal = new libterminal.Terminal({
+    terminal = new libterminal.Terminal({
       fontFamily: `"Inconsolata", "Noto Mono", "Source Code Pro", monospace`,
       fontWeight: "500",
       fontWeightBold: "700",
@@ -38,87 +41,58 @@
       title = t;
     });
 
+    const resizer = new ResizeObserver(() => terminal.fit());
+    resizer.observe(terminalElement);
+
     done(terminal);
 
     return () => {
+      resizer.disconnect();
       terminal.dispose();
       onTitleChange.dispose();
     };
   });
 </script>
 
-<div
-  {id}
-  class="terminal-box"
-  style="
-    --background: {combinedColors.background};
-    --foreground: {combinedColors.foreground};
-  "
->
-  <header>
-    <h3>{title} – xterm.js</h3>
-  </header>
-  <div class="terminal monospace" bind:this={terminalElement} />
-</div>
+<Window {id}>
+  <h3 slot="title">{title ? `${title} – xterm.js` : "xterm.js"}</h3>
+  <div
+    class="terminal-box monospace"
+    style="
+      --background: {combinedColors.background};
+      --foreground: {combinedColors.foreground};
+    "
+  >
+    <div class="terminal-box-content" bind:this={terminalElement} />
+  </div>
+</Window>
 
 <style>
   div.terminal-box {
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-
-    box-shadow: 0 2px 16px -6px rgba(0, 0, 0, 0.77);
-    border-radius: 15px 15px 0 0;
-  }
-
-  header {
-    color: black;
-    background: linear-gradient(
-      to right,
-      rgba(85, 205, 252, 1) 0%,
-      rgba(147, 194, 255, 1) 25%,
-      rgba(200, 181, 245, 1) 50%,
-      rgba(234, 171, 217, 1) 75%,
-      rgba(247, 168, 184, 1) 100%
-    );
-    text-align: center;
-  }
-
-  header h3 {
-    font-weight: bold;
-    font-size: 1rem;
-    margin: 0.75rem;
-    user-select: none;
-  }
-
-  div.terminal {
-    flex: 1;
-    padding: clamp(6px, 1.5vh, 12px) clamp(0px, 0.5vw, 4px);
+    height: 100%;
+    padding: clamp(4px, 1.5vh, 8px) clamp(0px, 0.5vw, 4px);
+    box-sizing: border-box;
     background-color: var(--background);
   }
 
-  div.terminal > :global(*) {
+  div.terminal-box div.terminal-box-content,
+  div.terminal-box :global(div.terminal),
+  div.terminal-box :global(div.xterm-viewport) {
     height: 100%;
   }
 
-  div.terminal :global(.xterm-screen) {
+  div.terminal-box :global(.xterm-screen) {
     margin: auto;
   }
 
-  div.terminal :global(.xterm-underline-5) {
+  div.terminal-box :global(.xterm-underline-5) {
     text-decoration: dotted underline !important;
     text-decoration-thickness: 0.05em !important;
   }
 
-  div.terminal
+  div.terminal-box
     :global(.xterm-underline-5[style="text-decoration: underline;"]) {
     text-decoration: underline !important;
     text-decoration-thickness: 0.05em !important;
-  }
-
-  @media (max-width: 500px) {
-    header h3 {
-      margin: 0.45rem;
-    }
   }
 </style>
---
+++ site/components/Window.svelte
@@ -0,0 +1,189 @@
+<script lang="ts">
+  import { viewDesktop } from "#/libdb.so/site/lib/views.js";
+  import WindowMinimize from "#/libdb.so/site/components/Papirus/window-minimize.svelte";
+  import WindowMaximize from "#/libdb.so/site/components/Papirus/window-maximize.svelte";
+  import WindowRestore from "#/libdb.so/site/components/Papirus/window-restore.svelte";
+
+  export let id = "";
+  export let maximized = false;
+
+  // minimize sets the minimize callback. If null, then the minimize button is
+  // hidden. If undefined, then the minimize button minimizes the window.
+  export let minimize: undefined | null | (() => void) = undefined;
+  // maximize behaves like minimize, except undefined means the button is
+  // visible but disabled.
+  export let maximize: undefined | null | (() => void) = undefined;
+
+  function onMinimize() {
+    if (minimize) {
+      minimize();
+      return;
+    }
+    viewDesktop();
+  }
+
+  function onMaximize() {
+    if (maximize) {
+      maximize();
+      return;
+    }
+    maximized = !maximized;
+  }
+</script>
+
+<div class="window-container" class:maximized>
+  <main class="window" class:maximized {id}>
+    <header class="titlebar">
+      <div class="title">
+        <slot name="title" />
+      </div>
+      <div class="controls">
+        {#if minimize !== null}
+          <button class="minimize" on:click={onMinimize}>
+            <WindowMinimize />
+          </button>
+        {/if}
+        {#if maximize !== null}
+          <button class="maximize" on:click={onMaximize}>
+            {#if maximized}
+              <WindowRestore />
+            {:else}
+              <WindowMaximize />
+            {/if}
+          </button>
+        {/if}
+      </div>
+    </header>
+    <div class="content">
+      <slot />
+    </div>
+  </main>
+</div>
+
+<style lang="scss">
+  div.window-container {
+    width: 100%;
+    height: 100%;
+
+    &:not(.maximized) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  main.window {
+    --window-border-color: rgba(255, 255, 255, 0.1);
+
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+
+    box-shadow: 0 2px 16px -6px rgba(0, 0, 0, 0.77);
+    border-radius: 12px;
+
+    outline: 1px solid var(--window-border-color);
+    outline-offset: -1px;
+
+    width: min(calc(100% - clamp(6px, 5vw, 3rem)), max(80vw, 1000px));
+    height: min(calc(100% - clamp(6px, 7vw, 5rem)), max(70vh, 800px));
+
+    @mixin maximize {
+      border-radius: 0;
+      width: 100%;
+      height: 100%;
+      outline: none;
+    }
+
+    &.maximized {
+      @include maximize;
+    }
+
+    @media (max-width: 500px) {
+      @include maximize;
+    }
+
+    .content {
+      flex: 1;
+      overflow: hidden;
+    }
+
+    header.titlebar {
+      color: white;
+      background-color: #030303;
+      border-bottom: 1px solid var(--window-border-color);
+
+      text-align: center;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      :global(h1),
+      :global(h2),
+      :global(h3) {
+        font-weight: bold;
+        font-size: 1rem;
+
+        margin: 0.65rem;
+        @media (max-width: 500px) {
+          margin: 0.45rem;
+        }
+      }
+
+      .title {
+        flex: 1;
+      }
+
+      .controls {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+
+        margin: 0 0.65rem;
+        @media (max-width: 500px) {
+          margin: 0 0.45rem;
+        }
+
+        button {
+          color: white;
+          border: none;
+          border-radius: 99px;
+          width: 1.5rem;
+          height: 1.5rem;
+          font-size: 0.75rem;
+          font-weight: 900;
+          line-height: 0;
+          background-color: rgba(255, 255, 255, 0.1);
+          transition: all 0.1s ease-in-out;
+          padding: 0;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          &:hover {
+            background-color: rgba(255, 255, 255, 0.2);
+          }
+
+          &.minimize {
+            :global(svg *) {
+              fill: rgba(85, 205, 252, 1);
+            }
+          }
+
+          &.maximize {
+            :global(svg *) {
+              fill: rgba(247, 168, 184, 1);
+            }
+            @media (max-width: 500px) {
+              /* Always maximize on mobile */
+              display: none;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
