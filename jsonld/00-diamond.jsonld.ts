8f3f0a9d1f319a57e7407a67e92e900f6b921a5c
diamondburned <diamond@libdb.so>
2025-07-23 21:26:42 -0700

    Fix umbresp.cat 88x31 URL

--- jsonld/00-diamond.jsonld.ts
+++ jsonld/00-diamond.jsonld.ts
@@ -83,7 +83,7 @@ const graph = [
       {
         "libdb:88x31Badge/alt": "umbresp.cat",
         "libdb:88x31Badge/link": "https://umbresp.cat",
-        "libdb:88x31Badge/image": _88x31("umbresp.cat.png"),
+        "libdb:88x31Badge/image": _88x31("umbresp.cat.gif"),
       },
       {
         "libdb:88x31Badge/alt": "nc pinecone",
