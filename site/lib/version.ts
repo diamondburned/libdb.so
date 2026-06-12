dd1b606949aff0742e619d9c7415121147fb09bf
diamondburned <diamond@libdb.so>
2026-06-12 02:34:24 -0700

    add version footer

--- site/lib/version.ts
+++
@@ -1,9 +0,0 @@
-import { execSync } from "child_process";
-
-export function version() {
-  try {
-    return execSync(`git describe --tags --always --dirty`).toString().trim();
-  } catch (error) {
-    return "unknown";
-  }
-}
